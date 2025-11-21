20b0d9ece8021bbbee2afdff03618f2f520a9a6b
Himanshu Bhatt <himanshu43.work@gmail.com>
2025-11-21 10:16:36 +0530

    backend directory created

---
+++ UserAuthMS/src/main/java/com/finsight/user/UserAuthMsApplication.java
@@ -0,0 +1,13 @@
+package com.finsight.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthMsApplication.class, args);
+	}
+
+}
---
+++ UserAuthMS/src/test/java/com/finsight/user/UserAuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsight.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
