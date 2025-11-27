125de20fbf6d0a822b14a5015962a942cc15993f
Himanshu Bhatt <himanshu43.work@gmail.com>
2025-11-27 22:54:49 +0530

    this is for now

--- UserAuthMS/src/main/java/com/finsight/user/services/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.auction.auth.services;
-
-import jakarta.mail.MessagingException;
-
-public interface EmailService {
-    void sendWelcomeEmail(String toEmail, String name);
-    void sendOtpEmail(String toEmail, String otp) throws MessagingException;
-    void sendResetOtpEmail(String toEmail, String otp) throws MessagingException;
-}
--- UserAuthMS/src/main/java/com/finsight/user/services/ProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.auction.auth.services;
-
-public interface ProfileService {
-    void sendOtp(String email);
-    void verifyOtp(String email, String otp);
-    void sendResetOtp(String email);
-    void resetPassword(String email, String otp, String newPassword);
-}
